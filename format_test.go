501849b21b2a0c8d2ed1a7145efb7c4371b5c7be
Yad Smood <y.s.inside@gmail.com>
2023-04-21 15:21:21 +0800

    remove got

--- format_test.go
+++ format_test.go
@@ -1,33 +1,88 @@
 package gop_test
 
 import (
+	"bytes"
+	"crypto/rand"
 	"encoding/base64"
+	"encoding/hex"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"reflect"
 	"testing"
+	"text/template"
 	"time"
 	"unsafe"
 
 	"github.com/ysmood/gop"
-	"github.com/ysmood/got"
-	"github.com/ysmood/got/lib/diff"
 )
 
+func eq(t *testing.T, a, b interface{}) {
+	t.Helper()
+	if a == b {
+		return
+	}
+	t.Log("should equal")
+	t.Fail()
+}
+
+func neq(t *testing.T, a, b interface{}) {
+	if a != b {
+		return
+	}
+	t.Log("should not equal")
+	t.Fail()
+}
+
+// RandStr generates a random string with the specified length
+func randStr(l int) string {
+	b := make([]byte, (l+1)/2)
+	_, _ = rand.Read(b)
+	return hex.EncodeToString(b)[:l]
+}
+
+func render(value string, data interface{}) string {
+	out := bytes.NewBuffer(nil)
+	t := template.New("")
+	t, err := t.Parse(value)
+	if err != nil {
+		panic(err)
+	}
+	err = t.Execute(out, data)
+	if err != nil {
+		panic(err)
+	}
+	return out.String()
+}
+
+func assertPanic(t *testing.T, fn func()) (val interface{}) {
+	t.Helper()
+
+	defer func() {
+		t.Helper()
+
+		val = recover()
+		if val == nil {
+			t.Error("should panic")
+		}
+	}()
+
+	fn()
+
+	return
+}
+
 func TestStyle(t *testing.T) {
-	g := got.T(t)
 	s := gop.Style{Set: "<s>", Unset: "</s>"}
 
-	g.Eq(gop.S("test", s), "<s>test</s>")
-	g.Eq(gop.S("", s), "<s></s>")
-	g.Eq(gop.S("", gop.None), "")
+	eq(t, gop.S("test", s), "<s>test</s>")
+	eq(t, gop.S("", s), "<s></s>")
+	eq(t, gop.S("", gop.None), "")
 }
 
 func TestTokenize(t *testing.T) {
-	g := got.T(t)
 	ref := "test"
 	timeStamp, _ := time.Parse(time.RFC3339Nano, "2021-08-28T08:36:36.807908+08:00")
 	fn := func(string) int { return 10 }
@@ -80,33 +135,49 @@ func TestTokenize(t *testing.T) {
 		[]byte(`{"a": 1}`),
 	}
 
-	check := func(out string, tpl string) {
-		g.Helper()
+	check := func(out string, file string) {
+		t.Helper()
 
-		expected := g.Render(tpl, map[string]interface{}{
+		tpl, err := os.ReadFile(file)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expected := render(string(tpl), map[string]interface{}{
 			"ch1": fmt.Sprintf("0x%x", reflect.ValueOf(ch1).Pointer()),
 			"ch2": fmt.Sprintf("0x%x", reflect.ValueOf(ch2).Pointer()),
 			"ch3": fmt.Sprintf("0x%x", reflect.ValueOf(ch3).Pointer()),
 			"fn":  fmt.Sprintf("0x%x", reflect.ValueOf(fn).Pointer()),
 			"ptr": fmt.Sprintf("%v", &ref),
 			"ref": fmt.Sprintf("0x%x", reflect.ValueOf(&ref).Pointer()),
-		}).String()
+		})
 
 		if out != expected {
-			g.Fail()
-			g.Log(diff.Diff(out, expected))
+			t.Log("check failed")
+			t.Fail()
 		}
 	}
 
 	out := gop.StripANSI(gop.F(v))
 
 	{
-		code := fmt.Sprintf(g.Read(filepath.Join("fixtures", "compile_check.go.tmpl")).String(), out)
-		f := filepath.Join("tmp", g.RandStr(8), "main.go")
-		g.WriteFile(f, code)
-		b, err := exec.Command("go", "run", f).CombinedOutput()
+		b, err := os.ReadFile(filepath.Join("fixtures", "compile_check.go.tmpl"))
 		if err != nil {
-			g.Error(string(b))
+			t.Fatal(err)
+		}
+		code := fmt.Sprintf(string(b), out)
+		f := filepath.Join("tmp", randStr(8), "main.go")
+		err = os.MkdirAll(filepath.Dir(f), 0755)
+		if err != nil {
+			t.Fatal(err)
+		}
+		err = os.WriteFile(f, []byte(code), 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+		b, err = exec.Command("go", "run", f).CombinedOutput()
+		if err != nil {
+			t.Fatal(string(b))
 		}
 	}
 
@@ -117,11 +188,10 @@ func TestTokenize(t *testing.T) {
 }
 
 func TestRef(t *testing.T) {
-	g := got.T(t)
 	a := [2][]int{{1}}
 	a[1] = a[0]
 
-	g.Eq(gop.Plain(a), `[2][]int{
+	eq(t, gop.Plain(a), `[2][]int{
     []int/* len=1 cap=1 */{
         1,
     },
@@ -142,12 +212,11 @@ type B struct {
 }
 
 func TestCircularRef(t *testing.T) {
-	g := got.T(t)
 	a := A{Int: 10}
 	b := B{"test", &a}
 	a.B = &b
 
-	g.Eq(gop.StripANSI(gop.F(a)), `gop_test.A{
+	eq(t, gop.StripANSI(gop.F(a)), `gop_test.A{
     Int: 10,
     B: &gop_test.B{
         s: "test",
@@ -162,7 +231,7 @@ func TestCircularRef(t *testing.T) {
 func TestCircularNilRef(t *testing.T) {
 	arr := []A{{}, {}}
 
-	got.T(t).Eq(gop.StripANSI(gop.F(arr)), `[]gop_test.A/* len=2 cap=2 */{
+	eq(t, gop.StripANSI(gop.F(arr)), `[]gop_test.A/* len=2 cap=2 */{
     gop_test.A{
         Int: 0,
         B: (*gop_test.B)(nil),
@@ -175,26 +244,24 @@ func TestCircularNilRef(t *testing.T) {
 }
 
 func TestCircularMap(t *testing.T) {
-	g := got.T(t)
 	a := map[int]interface{}{}
 	a[0] = a
 
 	ts := gop.Tokenize(a)
 
-	g.Eq(gop.Format(ts, gop.ThemeNone), `map[int]interface {}{
+	eq(t, gop.Format(ts, gop.ThemeNone), `map[int]interface {}{
     0: gop.Circular().(map[int]interface {}),
 }`)
 }
 
 func TestCircularSlice(t *testing.T) {
-	g := got.New(t)
 	a := [][]interface{}{{nil}, {nil}}
 	a[0][0] = a[1]
 	a[1][0] = a[0][0]
 
 	ts := gop.Tokenize(a)
 
-	g.Eq(gop.Format(ts, gop.ThemeNone), `[][]interface {}/* len=2 cap=2 */{
+	eq(t, gop.Format(ts, gop.ThemeNone), `[][]interface {}/* len=2 cap=2 */{
     []interface {}/* len=1 cap=1 */{
         []interface {}/* len=1 cap=1 */{
             gop.Circular(0, 0).([]interface {}),
@@ -207,8 +274,6 @@ func TestCircularSlice(t *testing.T) {
 }
 
 func TestCircularMapKey(t *testing.T) {
-	g := got.New(t)
-
 	a := map[interface{}]interface{}{}
 	b := map[interface{}]interface{}{}
 	a[&b] = b
@@ -216,111 +281,98 @@ func TestCircularMapKey(t *testing.T) {
 
 	ts := gop.Tokenize(b)
 
-	g.Eq(gop.Format(ts, gop.ThemeNone), g.Render(`map[interface {}]interface {}{
+	eq(t, gop.Format(ts, gop.ThemeNone), render(`map[interface {}]interface {}{
     (interface {})(nil)/* {{.a}} */: map[interface {}]interface {}{
         (interface {})(nil)/* {{.b}} */: gop.Circular().(map[interface {}]interface {}),
     },
 }`, map[string]interface{}{
 		"a": fmt.Sprintf("0x%x", reflect.ValueOf(&a).Pointer()),
 		"b": fmt.Sprintf("0x%x", reflect.ValueOf(&b).Pointer()),
-	}).String())
+	}))
 }
 
 func TestPlain(t *testing.T) {
-	g := got.T(t)
-	g.Eq(gop.Plain(10), "10")
+	eq(t, gop.Plain(10), "10")
 }
 
 func TestPlainMinify(t *testing.T) {
-	g := got.T(t)
 	a := map[int]interface{}{
 		1: "a",
 	}
 	b := map[int]interface{}{}
 	pa := gop.Plain(a)
 	pb := gop.Plain(b)
-	g.Eq(pa, "map[int]interface {}{\n    1: \"a\",\n}")
-	g.Eq(pb, "map[int]interface {}{}")
+	eq(t, pa, "map[int]interface {}{\n    1: \"a\",\n}")
+	eq(t, pb, "map[int]interface {}{}")
 }
 
 func TestP(t *testing.T) {
-	gop.Stdout = ioutil.Discard
+	gop.Stdout = io.Discard
 	_ = gop.P("test")
 	gop.Stdout = os.Stdout
 }
 
 func TestConvertors(t *testing.T) {
-	g := got.T(t)
-	g.Nil(gop.Circular(""))
+	eq(t, gop.Circular(""), nil)
 
-	s := g.RandStr(8)
-	g.Eq(gop.Ptr(s).(*string), &s)
+	s := randStr(8)
+	eq(t, *gop.Ptr(s).(*string), s)
 
 	bs := base64.StdEncoding.EncodeToString([]byte(s))
 
-	g.Eq(gop.Base64(bs), []byte(s))
+	eq(t, string(gop.Base64(bs)), string([]byte(s)))
 	now := time.Now()
-	g.Eq(gop.Time(now.Format(time.RFC3339Nano), 1234), now)
-	g.Eq(gop.Duration("10m"), 10*time.Minute)
+	eq(t, gop.Time(now.Format(time.RFC3339Nano), 1234).Unix(), now.Unix())
+	eq(t, gop.Duration("10m"), 10*time.Minute)
 
-	g.Eq(gop.JSONStr(nil, "[1, 2]"), "[1, 2]")
-	g.Eq(gop.JSONBytes(nil, "[1, 2]"), []byte("[1, 2]"))
+	eq(t, gop.JSONStr(nil, "[1, 2]"), "[1, 2]")
+	eq(t, string(gop.JSONBytes(nil, "[1, 2]")), string([]byte("[1, 2]")))
 }
 
 func TestGetPrivateFieldErr(t *testing.T) {
-	g := got.T(t)
-	g.Panic(func() {
+	assertPanic(t, func() {
 		gop.GetPrivateField(reflect.ValueOf(1), 0)
 	})
-	g.Panic(func() {
+	assertPanic(t, func() {
 		gop.GetPrivateFieldByName(reflect.ValueOf(1), "test")
 	})
 }
 
 func TestTypeName(t *testing.T) {
-	g := got.T(t)
-
 	type f float64
 	type i int
 	type c complex128
 	type b byte
 
-	g.Eq(gop.Plain(f(1)), "gop_test.f(1.0)")
-	g.Eq(gop.Plain(i(1)), "gop_test.i(1)")
-	g.Eq(gop.Plain(c(1)), "gop_test.c(1+0i)")
-	g.Eq(gop.Plain(b('a')), "gop_test.b(97)")
+	eq(t, gop.Plain(f(1)), "gop_test.f(1.0)")
+	eq(t, gop.Plain(i(1)), "gop_test.i(1)")
+	eq(t, gop.Plain(c(1)), "gop_test.c(1+0i)")
+	eq(t, gop.Plain(b('a')), "gop_test.b(97)")
 }
 
 func TestSliceCapNotEqual(t *testing.T) {
-	g := got.T(t)
-
 	x := gop.Plain(make([]int, 3, 10))
 	y := gop.Plain(make([]int, 3))
 
-	g.Desc("we should show the diff of cap").Neq(x, y)
+	neq(t, x, y)
 }
 
 func TestFixNestedStyle(t *testing.T) {
-	g := got.T(t)
-
 	s := gop.S(" 0 "+gop.S(" 1 "+
 		gop.S(" 2 "+
 			gop.S(" 3 ", gop.Cyan)+
 			" 4 ", gop.Blue)+
 		" 5 ", gop.Red)+" 6 ", gop.BgRed)
-	fmt.Println(gop.VisualizeANSI(s))
 	out := gop.VisualizeANSI(gop.FixNestedStyle(s))
-	g.Eq(out, `<41> 0 <31> 1 <39><34> 2 <39><36> 3 <39><34> 4 <39><31> 5 <39> 6 <49>`)
+	eq(t, out, `<41> 0 <31> 1 <39><34> 2 <39><36> 3 <39><34> 4 <39><31> 5 <39> 6 <49>`)
 
 	gop.FixNestedStyle("test")
 }
 
 func TestStripANSI(t *testing.T) {
-	g := got.T(t)
-	g.Eq(gop.StripANSI(gop.S("test", gop.Red)), "test")
+	eq(t, gop.StripANSI(gop.S("test", gop.Red)), "test")
 }
 
 func TestTheme(t *testing.T) {
-	g := got.T(t)
-	g.Eq(gop.ThemeDefault(gop.Error), []gop.Style{gop.Underline, gop.Red})
+	eq(t, gop.ThemeDefault(gop.Error)[0], gop.Underline)
 }
--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/ysmood/gop
 
 go 1.19
-
-require github.com/ysmood/got v0.32.0
